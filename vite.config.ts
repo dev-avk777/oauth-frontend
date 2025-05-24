9236a1e87d4000111eac49d6d7267ff09eb7e7d5
dev-avk <dev.avk777@gmail.com>
2025-05-24 15:32:44 +0300

    chore: update Vite configuration and GitHub Actions for deployment
    - Modified Vite configuration to allow connections from all interfaces and added CORS settings for production.
    - Updated GitHub Actions workflow to specify working directory for frontend build and environment variable creation.
    - Enhanced deployment script to generate the .env file for the frontend and reload Nginx after deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,27 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+
+  // для локальной разработки по http://localhost:3007 или http://172.26.0.4:3007
   server: {
+    host: '0.0.0.0',
     port: 3007,
   },
+
+  // для preview (vite preview), т.е. когда вы запускаете конечную сборку на prod-порту
   preview: {
-    port: 3007,
+    host: '0.0.0.0', // слушаем на всех интерфейсах
+    port: 3007, // порт проксирования
+    allowedHosts: [
+      // доверенные имена хостов
+      'tokenswallet.ru',
+      'www.tokenswallet.ru',
+      'localhost',
+      '127.0.0.1',
+    ],
+    cors: {
+      // CORS-настройки
+      origin: ['https://tokenswallet.ru'],
+    },
   },
 })
